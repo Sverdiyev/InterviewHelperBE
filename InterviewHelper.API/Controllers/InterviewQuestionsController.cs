9901070765902d12dd2c891e8e97345eabd19182
Ioseb Otskheli <ioseb.otskheli@devbridge.com>
2022-03-21 09:29:09 +0100

    Initial commit

---
+++ InterviewHelper.API/Controllers/InterviewQuestionsController.cs
@@ -0,0 +1,36 @@
+using InterviewHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewHelper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InterviewQuestionsController : ControllerBase
+    {
+        private static readonly string[] ProgrammingLanguages = new[]
+        {
+            "C#", "Java", "Javascript", "C++", "Python", "Go"
+        };
+
+        private readonly ILogger<InterviewQuestionsController> _logger;
+
+        public InterviewQuestionsController(ILogger<InterviewQuestionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAllQuestions")]
+        public IEnumerable<Question> Get()
+        {
+            return Enumerable.Range(0, 4).Select(index => new Question
+            {
+                Id = index,
+                CreationDate = DateTime.Now.AddDays(index),
+                Complexity = 0,
+                Language = ProgrammingLanguages[index],
+                Contetnt = $"This question is about '{ProgrammingLanguages[index]}' programming language!"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ InterviewHelper.Core/Models/Question.cs
@@ -0,0 +1,11 @@
+namespace InterviewHelper.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string? Language  { get; set; }
+        public int Complexity { get; set; }
+        public string Contetnt { get; set; }
+    }
+}
