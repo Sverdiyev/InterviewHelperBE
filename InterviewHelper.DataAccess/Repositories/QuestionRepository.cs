47a7b02157b7b21723b6b1800456d0c47832ea7a
WDev-Beginner <ab.serghine@gmail.com>
2022-03-29 23:52:22 +0200

    made the _questions list a local variable in the GetAllQuestions function.

--- InterviewHelper.DataAccess/Repositories/QuestionRepository.cs
+++ InterviewHelper.DataAccess/Repositories/QuestionRepository.cs
@@ -7,23 +7,22 @@ namespace InterviewHelper.DataAccess.Repositories;
 public class QuestionRepository: IQuestionRepository
 {
     private readonly QuestionFactory _factory;
-    private readonly List<Question> _questions;
 
     public QuestionRepository()
     {
-        _questions = new List<Question>();
         _factory = new QuestionFactory();
     }
     public IEnumerable<Question> GetAllQuestions()
     {
+        var questions = new List<Question>();
         var creationTimeStamp = new DateTime(2022, 3, 29, 10,30, 30);
 
         for (var i = 0; i < 50; i++)
         {
             creationTimeStamp = creationTimeStamp.AddHours(6);
-            _questions.Add(_factory.GenerateQuestion(i+1, creationTimeStamp));
+            questions.Add(_factory.GenerateQuestion(i+1, creationTimeStamp));
         }
 
-        return _questions;
+        return questions;
     }
 }
