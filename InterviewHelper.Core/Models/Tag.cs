8ba1794a598f85b0c0b09ff619b3845065c52478
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-24 11:28:20 +0200

    removed creation date from Tag

--- InterviewHelper.Core/Models/Tag.cs
+++ InterviewHelper.Core/Models/Tag.cs
@@ -3,6 +3,6 @@ namespace InterviewHelper.Core.Models;
 public class Tag
 {
     public int Id { get; set; }
-    public DateTime CreationDate { get; set; } = DateTime.Now;
+    public DateTime CreationDate { get; set; }
     public string TagName { get; set; }
 }
