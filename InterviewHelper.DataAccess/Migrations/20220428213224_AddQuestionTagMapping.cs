62ff316052c38ffeb8a14757a3ee2fb900331cca
WDev-Beginner <ab.serghine@gmail.com>
2022-04-28 23:36:34 +0200

    implemented question deletion and setup the mapping between the tags and the questions

---
+++ InterviewHelper.DataAccess/Migrations/20220428213224_AddQuestionTagMapping.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewHelper.DataAccess.Migrations
+{
+    public partial class AddQuestionTagMapping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tag_Questions_QuestionId",
+                table: "Tag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tag",
+                table: "Tag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tag_QuestionId",
+                table: "Tag");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionId",
+                table: "Tag");
+
+            migrationBuilder.RenameTable(
+                name: "Tag",
+                newName: "Tags");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tags",
+                table: "Tags",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "QuestionTag",
+                columns: table => new
+                {
+                    QuestionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TagId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionTag", x => new { x.QuestionId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_QuestionTag_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuestionTag_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionTag_TagId",
+                table: "QuestionTag",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionTag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tags",
+                table: "Tags");
+
+            migrationBuilder.RenameTable(
+                name: "Tags",
+                newName: "Tag");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionId",
+                table: "Tag",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tag",
+                table: "Tag",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_QuestionId",
+                table: "Tag",
+                column: "QuestionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tag_Questions_QuestionId",
+                table: "Tag",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id");
+        }
+    }
+}
