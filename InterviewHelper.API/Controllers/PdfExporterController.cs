cdc8c0ebe7182765d69382cf2d1ce0b4ec51526f
WDev-Beginner <ab.serghine@gmail.com>
2022-04-30 06:05:36 +0200

    implemented the initial version of the pdf exporter.

---
+++ InterviewHelper.API/Controllers/PdfExporterController.cs
@@ -0,0 +1,29 @@
+﻿using InterviewHelper.Core.Models.PdfExporterModels;
+using Microsoft.AspNetCore.Mvc;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace InterviewHelper.Api.Controllers;
+[ApiController]
+    
+[Route("[controller]")]
+public class PdfController : Controller
+{
+    
+    [HttpPost]
+    public IActionResult DownloadPdfFile(PdfExporterModel model)
+    {
+        IronPdf.Installation.LinuxAndDockerDependenciesAutoConfig = true;
+        var html = RenderRazorViewToString("myQuestionPdf", model);
+        var ironPdfRender = new IronPdf.ChromePdfRenderer();
+        using var pdfDoc = ironPdfRender.RenderHtmlAsPdf(html);
+        return File(pdfDoc.Stream.ToArray(), "application/pdf");
+    }
+    
+    private static string RenderRazorViewToString(string viewName, PdfExporterModel model)
+    {
+        var razorContent = System.IO.File.ReadAllText("../InterviewHelper.Core/Helper/PdfBaseTemplate.cshtml", System.Text.Encoding.UTF8);
+         
+        return RazorEngine.Engine.Razor.RunCompile(razorContent, viewName, typeof(PdfExporterModel), model);
+    }
+}
---
+++ InterviewHelper.Core/Models/PdfExporterModels/PdfExporterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InterviewHelper.Core.Models.PdfExporterModels;
+
+public class PdfExporterModel
+{
+    public int IntervieweeId { get; set; }
+    public DateTime InterviewDate { get; set; }
+    public string IntervieweePosition { get; set; }
+    public List<string> Questions { get; set; }
+}
