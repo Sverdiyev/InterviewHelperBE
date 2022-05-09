31ce4d8650fdb982289f9494addfd02b0ce2a7e6
WDev-Beginner <ab.serghine@gmail.com>
2022-05-09 22:26:27 +0200

    added enum constraint to userVote prop

--- InterviewHelper.Core/Models/Vote.cs
+++ InterviewHelper.Core/Models/Vote.cs
@@ -1,9 +1,19 @@
-﻿namespace InterviewHelper.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterviewHelper.Core.Models;
 
 public class Vote
 {
+    private enum VoteOptions
+    {
+        up,
+        down
+    }
+
     public int Id { get; set; }
     public int UserId { get; set; }
     public int QuestionId { get; set; }
+    
+    [EnumDataType(typeof(VoteOptions))]
     public string? UserVote { get; set; }
 }
